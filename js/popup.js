9e96d1be7e7c043abe31451fd7602fbfcb41e63f
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2024-03-27 14:03:19 +0700

    add: text chat blur

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('changeColor');
-    button.addEventListener('click', function () {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                function: changeWhatsAppColor
-            });
-        });
-    });
+    textChatBlurHandler()
 });
 
-function changeWhatsAppColor() {
-    document.body.style.backgroundColor = 'red';
+function textChatBlurHandler() {
+    var toggle_text = document.getElementById("text-chat");
+
+    toggle_text.addEventListener('change', function () {
+        if (this.checked) {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    function: () => {
+                        var _ak8j = document.body.querySelector('._ak8j');
+
+                        _ak8j.style.filter = 'blur(10px)';
+                    }
+                });
+            });
+        } else {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    function: () => {
+                        var _ak8j = document.body.querySelector('._ak8j');
+
+                        _ak8j.style.filter = 'blur(0px)';
+                    }
+                });
+            });
+        }
+    });
 }
